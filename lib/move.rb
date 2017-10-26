1493d8a3a5db7459629c1521a12579f5db506989
GG Learn <ozolsdeko@gmail.com>
2017-10-26 19:18:24 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,6 +15,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def move(board, position, char="X")
   puts position
+  puts input_to_index(position)
   puts char
   #board[input_to_index] = char
 end
